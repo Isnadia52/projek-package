df3b6d68c8100148282eec8d5ba9c894f7b9203a
Natasyacay1 <natasyacaca916@gmail.com>
2024-10-08 22:44:56 +0800

    add varians dan simpangan baku

--- src/varians.py
+++ src/varians.py
@@ -8,9 +8,8 @@ def hitung_varians_sampel(data):
     return varians 
 
 def hitung_varians_populasi(data):
-
-    # """ Menghitung varians populasi
-    # Rumus: o² = (1/n) * Σ(x_i - μ)² """
+    """ Menghitung varians populasi
+     Rumus: o² = (1/n) * Σ(x_i - μ)² """
 
     n = len(data)
     mean = (data) / n
@@ -24,4 +23,8 @@ def hitung_varians(data, jenis_varians):
         return hitung_varians_populasi(data)
     else:
         raise ValueError("Jenis Varians tidak valid. Gunakan 'sampel' atau 'populasi'  ")
-    
+
+def hitung_simpangan_baku(data, jenis_varians):
+    "Menghitung simpangan baku dari varians"
+    varians = hitung_varians(data, jenis_varians)
+    return varians **0.5 #Akar Kuadrat 
