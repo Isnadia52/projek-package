5ce9b7cbab7c7f91b58bdfb391fb4b8379c09322
Marchegatriani <marchegatrianisude@gmail.com>
2024-10-10 11:30:37 +0800

    menambahkan method median dan modus

--- src/main.py
+++ src/main.py
@@ -4,9 +4,6 @@ class StatisticMethod:
     datas = {}
 
     def __init__(self, file_path, sheet_name=0):
-        """ 
-        Marche
-        """
         try:
             data = pd.read_excel(file_path, sheet_name=sheet_name)
 
@@ -27,25 +24,38 @@ class StatisticMethod:
     def get_data(self, nama_kolom):
         return self.datas[nama_kolom]
 
-    def mean(self, column_name):
+    def mean(self, nama_kolom):
         '''Fungsi untuk menghitung rata-rata dari sekumpulan angka/nilai.'''
-        nilai = self.get_data(column_name)
+        nilai = self.get_data(nama_kolom)
         jumlah_nilai = sum(nilai)
         banyaknya_nilai = len(nilai)
         nilai_rata_rata = jumlah_nilai / banyaknya_nilai if banyaknya_nilai > 0 else 0
         return nilai_rata_rata
 
-    def median(self):
-        """ 
-        Marche
-        """
-        pass
+    def median(self, nama_kolom):
+        '''Fungsi untuk menentukan median dari sekumpulan nilai.'''
+        nilai = self.get_data(nama_kolom)
+        sorted_nilai = sorted(nilai)
+        banyaknya_nilai = len(sorted_nilai)
+        if banyaknya_nilai % 2 == 1:    # jika banyaknya nilai berjumlah ganjil
+            nilai_median = sorted_nilai[banyaknya_nilai // 2]
+            return nilai_median
+        else:                           # jika banyaknya nilai berjumlah genap
+            nilai_median = (sorted_nilai[banyaknya_nilai // 2 - 1] + sorted_nilai[banyaknya_nilai // 2]) / 2
+            return nilai_median
 
-    def modus(self):
-        """ 
-        Marche
-        """
-        pass
+    def modus(self, nama_kolom):
+        '''Fungsi untuk menentukan nilai modus dari sekumpulan nilai.'''
+        nilai = self.get_data(nama_kolom)
+        frekuensi = {}
+        for value in nilai:
+            if value in frekuensi:
+                frekuensi[value] += 1
+            else:
+                frekuensi[value] = 1
+        modus = max(frekuensi, key=frekuensi.get)
+        frekuensi_modus = frekuensi[modus]
+        return modus, frekuensi_modus
 
     def variansi(self):
         """ 
@@ -83,4 +93,3 @@ class StatisticMethod:
         pass
 
 #var1 = StatisticMethod("SAMPEL NILAI PROJEK ALGORITMA.xlsx")
-#print(var1.mean("Matematika"))
