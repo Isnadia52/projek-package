fb017ef5960912b7a282317c2e678ea599e972ba
Natasyacay1 <natasyacaca916@gmail.com>
2024-10-12 10:51:38 +0800

    bersihkan kode

--- src/main.py
+++ src/main.py
@@ -47,33 +47,31 @@ class StatisticMethod:
         """
         pass
 
-    def hitung_varians_sampel(self, nama_kolom):
+    def varians_sample(self, nama_kolom):
        """Rumus: s² = (1/(n-1)) * Σ(x_i - x̄)²"""
-       data = self.get_data(nama_kolom)
-       n = len(data) 
+       data = self.get_data(nama_kolom) 
        mean = self.mean(nama_kolom)
-       varians = sum((x - mean) ** 2 for x in data) / (n - 1) 
+       varians = sum((x - mean) ** 2 for x in data) / (len(data) - 1) 
        return varians 
     
-    def hitung_varians_populasi(self, nama_kolom):
+    def varians_population(self, nama_kolom):
        """ Rumus: o² = (1/n) * Σ(x_i - μ)² """
        data = self.get_data(nama_kolom)
-       n = len(data) 
        mean = self.mean(nama_kolom)
-       varians = sum((x - mean) ** 2 for x in data) / n 
+       varians = sum((x - mean) ** 2 for x in data) / len(data)
        return varians 
     
-    def hitung_varians(self, nama_kolom, jenis_varians):
+    def varians(self, nama_kolom, jenis_varians):
         if jenis_varians == 'sampel':
-            return self.hitung_varians_sampel(nama_kolom)
+            return self.varians_sample(nama_kolom)
         elif jenis_varians == 'populasi':
-            return self.hitung_varians_populasi(nama_kolom)
+            return self.varians_population(nama_kolom)
         else:
             raise ValueError("Jenis Varians tidak valid. Gunakan 'sampel' atau 'populasi'  ")
         
-    def hitung_standar_deviation(self, nama_kolom):
+    def standard_deviation(self, nama_kolom):
         """Menghitung simpangan baku dari varians"""
-        varians = self.hitung_varians_sampel(nama_kolom)
+        varians = self.varians_sample(nama_kolom)
         return varians ** 0.5  # Akar kuadrat
 
     def desil(self):
@@ -101,4 +99,4 @@ class StatisticMethod:
         pass
 
 var1 = StatisticMethod("src\\SAMPEL NILAI PROJEK ALGORITMA.xlsx")
-print(var1.hitung_varians_sampel("Matematika"))
+print(var1.hitung_varians("Matematika", "sampel"))
